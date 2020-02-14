0cb1f3f5a50b5674e1f05bf2812a2b16254af1f9
en30 <en30.git@gmail.com>
2020-02-15 04:02:52 +0900

    fixes POJ

--- chrome_extension/src/entry/poj.org.ts
+++ chrome_extension/src/entry/poj.org.ts
@@ -1,29 +1,23 @@
-// import { augment } from '../client';
+import { augment } from '../client';
 
-// const site = 'poj';
-// const id = location.search.match(/id=(.*?)(?:&|$)/m)[1];
+const site = 'poj';
+const id = location.search.match(/id=(.*?)(?:&|$)/m)[1];
+const timeLimit = parseInt(document.querySelector('.plm').textContent.match(/(\d+)MS/)[1], 10) * 1e6;
 
-// const parser = () => {
-//     const timeLimit = parseInt(document.querySelector('.plm').textContent.match(/(\d+)MS/)[1], 10) * 1e6;
-//     const sio = Array.from(document.querySelectorAll(".sio"));
-//     return {
-//         site,
-//         id,
-//         restriction: {
-//             timeLimit
-//         },
-//         testCases: [
-//             {
-//                 title: 'Sample',
-//                 input: sio[0].textContent,
-//                 output: sio[1].textContent
-//             }
-//         ]
-//     };
-// };
+const sio = Array.from(document.querySelectorAll(".sio"));
+const testCases = [
+    {
+        title: 'Sample',
+        input: sio[0].textContent,
+        output: sio[1].textContent
+    }
+]
 
-// const submit = (body: string) => {
-//     console.log(body);
-// }
-
-// augment(document, site, id, parser, submit);
+augment(document, {
+    site,
+    id,
+    restriction: {
+        timeLimit,
+    },
+    testCases,
+});
