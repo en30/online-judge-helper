9905ba99de4024f34a6d3c9cf03ac4292a72b573
en30 <en30.git@gmail.com>
2020-03-05 23:09:32 +0900

    fixes duplicated testcases on atcoder.jp

--- chrome_extension/src/entry/atcoder.jp.ts
+++ chrome_extension/src/entry/atcoder.jp.ts
@@ -8,18 +8,20 @@ const timeLimit = parseInt(document.querySelector('#task-statement').previousEle
 // ABC 009~?
 const normalizeNewline = (str: string) => str.trimRight() + "\n";
 
-const testCases = Array.from(document.querySelectorAll("h3")).reduce((a, el) => {
-    if (!el.textContent.match(/入力例|Sample Input/)) return a;
+const headers = Array.from(document.querySelectorAll("h3"));
+const examplePattern = headers.some((e) => e.textContent.match(/入力例/)) ? "入力例" : "Sample Input";
+
+const testCases = headers.reduce((a, el) => {
+    if (!el.textContent.match(examplePattern)) return a;
     const title = el.childNodes[0].textContent;
     const input = el.parentElement.querySelector('pre:last-of-type');
-    let e = input.closest('.part');
-    if (e) {
+    let e = input.closest('section') as Element;
+    if (e.nextElementSibling && e.nextElementSibling.tagName === 'SECTION') {
+        e = e.nextElementSibling;
+    } else {
+        e = input.closest('.part') as Element;
         e = e.nextElementSibling;
         while (!e.classList.contains('part')) e = e.nextElementSibling;
-    } else {
-        // ABC034
-        e = input.closest('section');
-        e = e.nextElementSibling;
     }
     const output = e.querySelector('pre[id^=for_copy],pre[id^=pre-sample],pre:last-of-type');
     a.push({
@@ -52,5 +54,5 @@ const s = document.createElement('script');
 s.src = chrome.runtime.getURL('code_mirror.js');
 s.addEventListener('load', function() {
     this.remove();
-})
+});
 document.documentElement.appendChild(s);
